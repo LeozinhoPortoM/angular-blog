bfeb7d38882e73288da03420e1f046b2868c7cb2
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2022-11-07 22:13:07 -0300

    Criando content e deixando as páginas dinamicas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover":
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnkKCZP-Zs7kwb_wyx3kYcKMLxlKBtLtZsAQ&usqp=CAU",
+    "title": "Homem-Aranha: sem volta para casa.",
+    "description":
+      'Chamado de "The more fun stuff version" ("A versão mais divertida", em tradução livre), o filme do herói com cenas novas tem previsão de estreia em 2 de setembro nos dois países. De acordo com a publicação, outros países serão divulgados em breve.',
+  },
+  {
+    "id": '2',
+    "photoCover":
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0l8G9eGGZlw2e6TCYDAFE9BtiBroTQbxHEQ&usqp=CAU',
+    "title": 'Novo filme do homem aranha',
+    "description":
+      'Não perca o novo filme do homem aranha, em breve vai tá nos cinemas.',
+  },
+  {
+    "id": '3',
+    "photoCover":
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsumQfAwCrXvQhchCvLCa5UBJUjN8JNxD0yQ&usqp=CAU',
+    "title": 'Homem de ferro volta para os cinemas',
+    "description":
+      'Não perca o novo filme do homem de ferro, em breve vai tá nos cinemas.',
+  },
+  {
+    "id": '4',
+    "photoCover":
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ5B_I1ZLBYMhXPRh7B35XeAMOtllpp-WkIA&usqp=CAU',
+    "title": 'Novo filme do Hulk',
+    "description":
+      'Não perca o novo filme do Hulk, em breve vai tá nos cinemas.',
+  },
+];
